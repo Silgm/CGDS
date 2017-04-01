79ff3cba11155b4dfecb3a025181618ce3af5843
Silgm <609630908@qq.com>
2017-04-01 16:33:11 +0800

    Build CGDS repository.

---
+++ CGDS/CGDS/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <assert.h>
+
+
+
+
+int main()
+{
+	printf("Finish Git Test!\n");
+	return EXIT_SUCCESS;
+}
